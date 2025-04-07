6d64755a44deac88a4ff11b417ae87906f46221d
AlexandrBezluckiy <a.bezluckiy@gmail.com>
2025-04-08 06:59:26 +0700

    commit

---
+++ src/JavaLearn/family.java
@@ -0,0 +1,7 @@
+package JavaLearn;
+
+public class family {
+    public static void family() {
+        System.out.println("Father\nMother\nSister\nI\nDog\nCat");
+    }
+}
---
+++ src/MyFamily.java
@@ -0,0 +1,7 @@
+import JavaLearn.family;
+
+public class MyFamily {
+    public static void main(String[] args) {
+        family.family();
+    }
+}
