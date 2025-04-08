284abc003edd490bb0a6ab1995975871fba11240
AlexandrBezluckiy <a.bezluckiy@gmail.com>
2025-04-08 10:13:15 +0700

    comit

---
+++ src/Char_type.java
@@ -0,0 +1,12 @@
+public class Char_type {
+    public static void main(String[] args) {
+/*
+        char ch1 = 88;
+        char ch2 = 'Y';
+        System.out.println("ch1 - " + ch1 + " ch2 - " + ch2);
+*/
+        for (char i = 0; i < 256; i++) {
+            System.out.println((int)i + " - " + i);
+        }
+    }
+}
