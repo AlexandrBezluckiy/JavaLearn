38dba630b0932f61c0cc8a08b52dd5057c598ce2
AlexandrBezluckiy <a.bezluckiy@gmail.com>
2025-04-09 06:43:26 +0700

    comit

--- src/Loop_while.java
+++ src/Loop_while.java
@@ -1,12 +1,14 @@
 public class Loop_while {
     public static void main(String[] args) {
 /*
+// Выводим значения от 0 до 1000
         int i = 1;
         while (i <= 1000) {
             System.out.println(i);
             i++;
         }
 
+// Выводим только положительные значения для переменной типа byte используя переполнение переменной и логическую переменную
         byte a = 0;
         boolean b = a >= 0;
         while (b) {
@@ -16,7 +18,9 @@ public class Loop_while {
                 b = false;
             }
         }
-*/
+
+// Выводим четные числа от 1000 до 0
+
         int i = 1000;
         while (i >= 0) {
             if (i % 2 == 0) {
@@ -24,5 +28,14 @@ public class Loop_while {
             }
             i--;
         }
+*/
+// Считаем среднее арифметическое всех чисел от 0 до 100
+        int i = 0;
+        int sum = 0;
+        while (i <= 100) {
+            sum += i;
+            i++;
+        }
+        System.out.println((float) sum/i);
     }
 }
