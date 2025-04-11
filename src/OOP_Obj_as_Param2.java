4a0cc53a57cfd30e3dbf96e010b9b27fe4c36b63
AlexandrBezluckiy <a.bezluckiy@gmail.com>
2025-04-12 01:31:23 +0700

    comit

---
+++ src/OOP_Obj_as_Param2.java
@@ -0,0 +1,15 @@
+import JavaLearn.ObjBox;
+
+public class OOP_Obj_as_Param2 {
+    public static void main(String[] args) {
+        ObjBox box1 = new ObjBox(5, 5, 5);
+        ObjBox box2 = new ObjBox(5, 5, 5);
+
+        String out = ObjBox.volume_compare(box1, box2);
+        System.out.println(out);
+
+        box2 = box2.resize_box(box1, 2);
+        System.out.println(box1 + "," + box2); // проверяем что это разные объекты;
+        System.out.println(ObjBox.show_param(box2));
+    }
+}
