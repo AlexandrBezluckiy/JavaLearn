de09ea2ea5eac311ab29811f4972e60af9f9daf6
AlexandrBezluckiy <a.bezluckiy@gmail.com>
2025-04-11 23:42:56 +0700

    comit

---
+++ src/JavaLearn/Method.java
@@ -0,0 +1,16 @@
+package JavaLearn;
+
+public class Method {
+    public static  double calc_mid(double a, double b) {
+        double res = (a + b)/2;
+        return res;
+    }
+    public static double calc_mid(double a, double b, double c) {
+        double res = (a + b + c)/3;
+        return res;
+    }
+    public static double calc_mid(double a, double b, double c, double d) {
+        double res = (a + b + c + d) / 4;
+        return res;
+    }
+}
---
+++ src/OOP_Overload_Method.java
@@ -0,0 +1,8 @@
+import JavaLearn.Method;
+
+public class OOP_Overload_Method {
+    public static void main(String[] args) {
+        System.out.println(Method.calc_mid(2, 4));
+        System.out.println(Method.calc_mid(3, 4, 6));
+    }
+}
