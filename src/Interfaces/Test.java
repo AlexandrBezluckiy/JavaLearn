a5e92849c16b0902bb7d7608204e17dfed536e26
AlexandrBezluckiy <a.bezluckiy@gmail.com>
2025-04-15 06:38:28 +0700

    comit

--- src/Interfaces/Test.java
+++ src/Interfaces/Test.java
@@ -2,7 +2,7 @@ package Interfaces;
 
 public class Test {
     public static void main(String[] args) {
-/*
+/* 1
         Animal animal1 = new Animal(1);
         Person person1 = new Person("Ikalay");
 
@@ -13,14 +13,23 @@ public class Test {
         person1.showInfo();
 */
 
+/* 2
         Info info1 = new Animal(1);
         Info info2 = new Person("Dick");
-/*
+//2.1
         info1.showInfo();
         info2.showInfo();
-*/
+//
+
         outputInfo(info1);
         outputInfo(info2);
+*/
+// 3
+        Animal animal1 = new Animal(1);
+        Person person1 = new Person("Ualera");
+
+        outputInfo(animal1); // на вход методу showInfo подается объект реализующий интерфейс Info, определенный для каждого класса.
+        outputInfo(person1); //
     }
 
     public static void outputInfo(Info info) {
